64853a7c1f6bf81b109ddcfc36fabec088ae773e
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-10-09 19:23:13 +0200

    created a CSS style for the main page

---
+++ main.js
@@ -0,0 +1,8 @@
+/* - 
+ Zobacz optymalny układ witryny w zależności od rozmiaru ekranu urządzenia
+- Zobacz stan najechania dla wszystkich interaktywnych elementów na stronie
+- Otwórz galerię lightbox, klikając duży obraz produktu
+- Przełącz duży obraz produktu, klikając małe miniatury zdjęć
+- Dodaj przedmioty do koszyka
+- Zobacz koszyk i usuń z niego przedmioty
+ */
