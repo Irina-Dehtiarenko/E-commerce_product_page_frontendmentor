a2885ee42abbcf556f66b5f9077b87a89d54896e
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-10-17 19:16:04 +0200

    created a function with correcting amount of order and adding to theahopping cart

--- main.js
+++ main.js
@@ -10,8 +10,8 @@
 // 3.1 (if strong amount < 0 - return, if strong amount === 0  - add .hidden)remove hidded(div.amount-of-order) amount +=1
 // 3.2 strong.amount +=1
 
-let amount = 2;
-let price = 125;
+let amount = 0;
+const price = 125;
 const buttonCart = document.querySelector("button.cart");
 const divCart = document.querySelector("div.cart");
 const amounOfOrder = document.querySelector(".amount-of-order");
@@ -25,21 +25,27 @@ const navigation = document.querySelector(".nav-mobile");
 const iconCloseNav = document.querySelector(
   ".opened-nav-menu .contener_icon-close .icon-close"
 );
-console.log(iconCloseNav);
+
+const buttonMinus = document.querySelector("button.minus");
+const buttonPlus = document.querySelector("button.plus");
+const buttonAddToCart = document.querySelector("button.add");
+
+const buttonDelete = document.querySelector("div.product svg");
+console.log(buttonDelete);
 
 // odrazu po zaladowaniu strony:
 selectedAmount.textContent = amount;
 
 // show the amoun of order in cart(powinno się zmieniać po klinknięciu w button)
 
-if (amount >= 0) {
-  if (amount === 0) {
-    amounOfOrder.classList.add("hidden");
-  } else {
-    amounOfOrder.classList.remove("hidden");
-    amounOfOrder.textContent = amount;
-  }
-}
+// if (amount >= 0) {
+//   if (amount === 0) {
+//     amounOfOrder.classList.add("hidden");
+//   } else {
+//     amounOfOrder.classList.remove("hidden");
+//     amounOfOrder.textContent = amount;
+//   }
+// }
 
 showMobNav = () => {
   navigation.classList.remove("hidden");
@@ -52,20 +58,42 @@ showMobNav = () => {
 iconMenu.addEventListener("click", showMobNav);
 
 const showCart = () => {
-  if (amount >= 0) {
-    divCart.classList.toggle("hidden");
-    if (amount === 0) {
-      cartEmpty.classList.remove("hidden");
-      cartNotEmpty.classList.add("hidden");
-      amounOfOrder.classList.add("hidden");
-    } else {
-      cartEmpty.classList.add("hidden");
-      cartNotEmpty.classList.remove("hidden");
-      amounOfOrder.classList.remove("hidden");
-      amounOfOrder.textContent = amount;
-      amountInCart.textContent = amount;
-      fullPrice.textContent = `$${price * amount}.00`;
-    }
+  divCart.classList.toggle("hidden");
+
+  if (!divCart.className.includes("hidden")) {
+    buttonDelete.addEventListener("click", deleteOrder);
   }
 };
 buttonCart.addEventListener("click", showCart);
+
+buttonMinus.addEventListener("click", () => {
+  if (amount <= 0) {
+    return;
+  } else {
+    amount -= 1;
+    selectedAmount.textContent = amount;
+  }
+});
+
+buttonPlus.addEventListener("click", () => {
+  amount += 1;
+  selectedAmount.textContent = amount;
+});
+
+const addToCart = () => {
+  divCart.classList.add("hidden");
+  if (amount === 0) {
+    cartEmpty.classList.remove("hidden");
+    cartNotEmpty.classList.add("hidden");
+    amounOfOrder.classList.add("hidden");
+  } else {
+    cartEmpty.classList.add("hidden");
+    cartNotEmpty.classList.remove("hidden");
+    amounOfOrder.classList.remove("hidden");
+    amounOfOrder.textContent = amount;
+    amountInCart.textContent = amount;
+    fullPrice.textContent = `$${price * amount}.00`;
+  }
+};
+
+buttonAddToCart.addEventListener("click", addToCart);
